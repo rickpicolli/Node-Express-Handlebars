24bdb6ee7686f6532b72982ec1b53f1df891ecf2
rickpicolli <ricardopicolli@gmail.com>
2018-03-05 21:22:49 -0800

    last details to make it work

--- config/orm.js
+++ config/orm.js
@@ -49,7 +49,7 @@ var orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
-    console.log(queryString);
+    console.log("this is querystring" + queryString);
 
     connection.query(queryString, vals, function(err, result) {
       if (err) {
